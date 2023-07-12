c2468c4cc31f48f697f5ab86220e2747bf128510
MGarcia510 <californiasunset33@gmail.com>
2023-07-12 14:34:57 -0700

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text (":|")
+                .font(.title)
+                .foregroundColor(Color.black)
+                .multilineTextAlignment(.leading)
+                .lineLimit(0)
+            Image("dog")
+                .renderingMode(.original)
+                .resizable(capInsets: EdgeInsets(top: 1.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .aspectRatio(contentMode: .fit)
+                
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
